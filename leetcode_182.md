38f3d8bb287ac15334d00013aea5b4a905e39fe0
Akhilesh Chand <64408106+developer-Akhil@users.noreply.github.com>
2025-04-12 10:26:46 +0530

    Update and rename leetcode_182.sql to leetcode_182.md

---
+++ leetcode_182.md
@@ -0,0 +1,7 @@
+# Descirpation
+
+https://leetcode.com/problems/duplicate-emails/description/
+
+# Solution
+
+select email from leetcode.person_182 group by email having count(email) >= 2;
--- leetcode_182.sql
+++
@@ -1,9 +0,0 @@
-Create table If Not Exists leetcode.Person_182 (id int, email varchar(255));
-
-Truncate table leetcode.Person_182;
-
-insert into leetcode.Person_182 (id, email) values ('1', 'a@b.com');
-insert into leetcode.Person_182 (id, email) values ('2', 'c@d.com');
-insert into leetcode.Person_182 (id, email) values ('3', 'a@b.com');
-
-select email from leetcode.Person_182 group by email having count(email) >= 2;
